78f02020322415b126e771b90790a90bf1b0800c
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-08-16 23:29:32 -0300

    Translating to english

--- stack.c
+++ stack.c
@@ -3,38 +3,38 @@
 #include <stdbool.h>
 #include <string.h>
 
-typedef struct Pilha {
-  unsigned int tamanho;
-  int topo;
-  int* pilha;
-}Pilha;
+typedef struct Stack {
+  unsigned int size;
+  int top;
+  int* buffer;
+}Stack;
 
-Pilha* criarPilha();
+Stack* createStack();
 int pop();
 _Bool push();
-_Bool vazio();
-int topo();
-void imprimirPilha();
+_Bool empty();
+int top();
+void printStack();
 void interface();
 
 int main (int argc, char* argv[]){
 
   char str[28];
-  char comando[15];
-  char argumento[10];
+  char command[15];
+  char argument[10];
 
-  unsigned int tamanho = (unsigned int) atoi(argv[1]);
+  unsigned int size = (unsigned int) atoi(argv[1]);
 
-  Pilha* pilha = criarPilha(tamanho);
+  Stack* buffer = createStack(size);
 
-  interface(pilha);
+  interface(buffer);
 }
 
-void interface(Pilha* pilha){
+void interface(Stack* buffer){
   
   char str[28];
-  char comando[15];
-  char argumento[10];
+  char command[15];
+  char argument[10];
 
 
   while(1){
@@ -48,78 +48,78 @@ void interface(Pilha* pilha){
  
     if(str[0] == '\n') continue;
  
-    while(str[i]!='(')  comando[j++] = str[i++];
+    while(str[i]!='(')  command[j++] = str[i++];
  
     i++;
     j = 0;
  
     int n;
  
-    while(str[i]!=')')  argumento[j++] = str[i++];
+    while(str[i]!=')')  argument[j++] = str[i++];
  
-    n = atoi(argumento);
+    n = atoi(argument);
  
-    if (!strcmp(comando, "quit")) exit(0);
-    else if (!strcmp(comando, "pop")) pop(pilha);
-    else if (!strcmp(comando, "push")) push(pilha, n);
-    else if (!strcmp(comando, "vazio")) printf("%d\n", vazio(pilha));
-    else if (!strcmp(comando, "topo")) printf("%d\n", topo(pilha));
-    else if (!strcmp(comando, "imprimirPilha")) imprimirPilha(pilha);
+    if (!strcmp(command, "quit")) exit(0);
+    else if (!strcmp(command, "pop")) pop(buffer);
+    else if (!strcmp(command, "push")) push(buffer, n);
+    else if (!strcmp(command, "empty")) printf("%d\n", empty(buffer));
+    else if (!strcmp(command, "top")) printf("%d\n", top(buffer));
+    else if (!strcmp(command, "printStack")) printStack(buffer);
     else {
-      printf("Comando inválido.\n");
+      printf("Invalid Command.\n");
     }
  
-    memset(comando, 0, sizeof(comando));
-    memset(argumento, 0, sizeof(argumento));
+    memset(command, 0, sizeof(command));
+    memset(argument, 0, sizeof(argument));
  }
 }
 
-Pilha* criarPilha(unsigned int tamanho){
-  Pilha* pilha = (Pilha*) malloc(tamanho * sizeof(Pilha));
-  pilha->tamanho = tamanho;
-  pilha->topo = -1;
-  pilha->pilha = (int*) malloc(tamanho * sizeof(int));
-  return pilha;
+Stack* createStack(unsigned int size){
+  Stack* buffer = (Stack*) malloc(size * sizeof(Stack));
+  buffer->size = size;
+  buffer->top = -1;
+  buffer->buffer = (int*) malloc(size * sizeof(int));
+  return buffer;
 }
 
-int pop(Pilha* pilha){
-  if (!vazio()){
-    pilha->topo--;
-    imprimirPilha(pilha);
+int pop(Stack* buffer){
+  if (!empty()){
+    buffer->top--;
+    printStack(buffer);
     return 1;
   }
 
-  else printf("A pilha está vazia.\n");
+  else printf("The stack is empty.\n");
   return 0;
 }
 
-_Bool push(Pilha* pilha, int n){
+_Bool push(Stack* buffer, int n){
   
-  if (pilha->topo == pilha->tamanho-1){
-    printf("A pilha está cheia.\n");
+  if (buffer->top == buffer->size-1){
+    printf("The stack is full.\n");
     return false;
   }
   
-  pilha->pilha[++pilha->topo] = n;
-  imprimirPilha(pilha);
+  buffer->buffer[++buffer->top] = n;
+  printStack(buffer);
   return true; 
 }
 
-_Bool vazio(Pilha* pilha){
-  if(pilha->topo == -1) return true;
+_Bool empty(Stack* buffer){
+  if(buffer->top == -1) return true;
   else return false;
 }
 
-int topo(Pilha* pilha){
-  return pilha->pilha[pilha->topo];
+int top(Stack* buffer){
+  return buffer->buffer[buffer->top];
 }
 
-void imprimirPilha(Pilha* pilha){
+void printStack(Stack* buffer){
   unsigned int i;
   printf("[");
-  for (i = 0; i < pilha->topo+1; i++){
-    printf("%u", pilha->pilha[i]);
-    if (i != pilha->topo) printf(", ");
+  for (i = 0; i < buffer->top+1; i++){
+    printf("%u", buffer->buffer[i]);
+    if (i != buffer->top) printf(", ");
   }
   printf("]\n");
 }
