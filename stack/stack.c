081255ee0d394644a27111709f56ee75fba9d176
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-08-19 20:25:15 -0300

    Stack - Changing terminal include calls

--- stack/stack.c
+++ stack/stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../terminal.h"
+#include "../c-terminal/terminal.h"
 
 //////////////////////////////////////////////////////////////////////
 
